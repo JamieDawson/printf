bde5e13f3a138a37e6c1d294831e73bd72cda086
Jamie Dawson <jadawson@e1z3r6p9.42.us.org>
2018-07-17 15:06:48 -0700

    test

--- src/flag_apply.c
+++ src/flag_apply.c
@@ -6,7 +6,7 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 20:19:01 by jadawson          #+#    #+#             */
-/*   Updated: 2018/07/17 12:28:19 by jadawson         ###   ########.fr       */
+/*   Updated: 2018/07/17 15:06:11 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 int		flag_apply_str(char *ret, int *bc, t_fwpl *modder)
 {
+	int		len;
+
+	len = ft_strlen(ret);
+	if (modder->width)
+		width_print_str(len, modder->width, bc, modder);
 	if (modder->flags & minus)
 		precision_str_print(ret, bc, modder);
+	ft_putstr_bc(ret, bc);
 	return (*bc);
 }
 
--- src/str_funct.c
+++ src/str_funct.c
@@ -6,7 +6,7 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/26 20:33:33 by jadawson          #+#    #+#             */
-/*   Updated: 2018/07/17 14:35:11 by jadawson         ###   ########.fr       */
+/*   Updated: 2018/07/17 15:03:38 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ int		str_print(va_list ap, t_fwpl *modder)
 		return(length_apply_str(ap, modder));
 	ret = va_arg(ap, char *);
 	len = ft_strlen(ret);
-	if (modder->flags)
+	if (modder->flags || modder->width)
 		return(flag_apply_str(ret, &bc, modder));
 	//width might break it.
-	if (modder->width)
-		width_print_str(len, modder->width, &bc, modder);
+	//if (modder->width)
+	//	width_print_str(len, modder->width, &bc, modder);
 	if (!ret)
 		ft_putstr("(null)");
 	ft_putstr_bc(ret, &bc);
