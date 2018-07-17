c4e3a550a9fe4cb9b81f537c70a919892a5ece81
Jamie Dawson <jadawson@e1z3r6p5.42.us.org>
2018-07-16 19:05:36 -0700

    updated Makefile

--- src/length_apply.c
+++ src/length_apply.c
@@ -6,14 +6,15 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 18:21:06 by jadawson          #+#    #+#             */
-/*   Updated: 2018/07/16 18:31:58 by jadawson         ###   ########.fr       */
+/*   Updated: 2018/07/16 19:05:01 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../ft_printf.h"
+#include "ft_printf.h"
 
 int		length_apply(va_list ap, t_fwpl *modder)
 {
 	if ((modder->length & l) == l)
 		return (s_wide_funct(ap, modder));
+	return (0);
 }
