951be3fac648130f899c9e04c3b20e0e4b153fd6
Jamie Dawson <jadawson@e1z3r6p5.42.us.org>
2018-07-16 20:28:23 -0700

    holding flags

---
+++ src/flag_apply.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flag_apply.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/16 20:19:01 by jadawson          #+#    #+#             */
+/*   Updated: 2018/07/16 20:26:36 by jadawson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		flag_apply(char *ret, int *bc, t_fwpl *modder)
+{
+	if (modder->flags & minus)
+	{
+		precision_str_print(ret, bc, modder);
+		return (*bc);
+	}
+	return (0);
+}
+
